4825777886fad3ed1e31d41d9519d5e6d51b231c
Zach Martin <zach@zach-martin.com>
2023-04-07 19:59:05 -0400

    Added sword animation

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     private PlayerControls _playerControls;
+    private Animator _animator;
+    
+    private static readonly int Attack = Animator.StringToHash("Attack");
 
     private void Awake()
     {
-        _playerControls = GetComponent<PlayerControls>();
+        _playerControls = new PlayerControls();
+        _animator = GetComponent<Animator>();
     }
 
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+    
     private void Start()
     {
-        
+        _playerControls.Combat.Attack.started += _ => TriggerAttack();
     }
 
+    private void TriggerAttack()
+    {
+        _animator.SetTrigger(Attack);
+    }
+    
     private void Update()
     {
         
