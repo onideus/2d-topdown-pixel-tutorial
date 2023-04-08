25bbaa178413001c4a7a303dd9ad287fba31d267
Zach Martin <zach@zach-martin.com>
2023-04-07 20:24:52 -0400

    Adding sword animation

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -7,32 +7,46 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls _playerControls;
     private Animator _animator;
-    
+    private PlayerController _playerController;
+    private ActiveWeapon _activeWeapon;
+
     private static readonly int Attack = Animator.StringToHash("Attack");
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
         _animator = GetComponent<Animator>();
+        _playerController = GetComponentInParent<PlayerController>();
+        _activeWeapon = GetComponentInParent<ActiveWeapon>();
     }
 
     private void OnEnable()
     {
         _playerControls.Enable();
     }
-    
+
     private void Start()
     {
         _playerControls.Combat.Attack.started += _ => TriggerAttack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
     }
 
     private void TriggerAttack()
     {
         _animator.SetTrigger(Attack);
     }
-    
-    private void Update()
+
+    private void MouseFollowWithOffset()
     {
-        
+        var mousePosition = Input.mousePosition;
+        var playerScreenPoint = Camera.main.WorldToScreenPoint(_playerController.transform.position);
+
+        _activeWeapon.transform.rotation = mousePosition.x < playerScreenPoint.x
+            ? Quaternion.Euler(0, -180, 0)
+            : Quaternion.Euler(0, 0, 0);
     }
 }
